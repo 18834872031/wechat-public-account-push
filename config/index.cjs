5d109852c1b709d22e47b71ccb854599b7e55c8d
fuwaeraaa <102898775+Wanda12138@users.noreply.github.com>
2024-01-29 13:40:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oY9lq63VvOX_o8AAHMJGBkRupu5c',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rTl0nOWMUX4KqJAvFutvhA7LRhhg1fjv9VRVvO1pxVI',
+      useTemplateId: 'f4TaSoQJXhg17KVxbX5idxh_vr6NSmrYrPcZCBZKruo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-09',
       festivals: [
