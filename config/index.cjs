08f899f255ba54603dfbc7dd46a8afc0a14ccd59
18834872031 <89234706+18834872031@users.noreply.github.com>
2024-04-03 10:17:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '春风',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oY9lq63VvOX_o8AAHMJGBkRupu5c',
+      id: 'onuIq6n-f2BUXQreM9R-hOJhkDNo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'poX-2ZIfSHGB7K5UB2Pj4l7rF6uP8wuXOsP8luyIUx0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
